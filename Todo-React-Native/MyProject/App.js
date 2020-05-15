24997142586f46648cf3052bbd65d94ee892de5d
Prem Kumar <prembammidi25@msitprogram.net>
2020-05-15 18:03:50 +0530

    mob-todo

---
+++ Todo-React-Native/MyProject/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoitem';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  }
+});
+
+function Todo() {
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: '1' },
+    { text: "create an app", key: '2' },
+    { text: "play on to switch", key: '3' },
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    })
+  }
+
+  return (
+    <View style={StyleSheet.container}>
+      <Header />
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler = {pressHandler}/>
+            )}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default Todo
